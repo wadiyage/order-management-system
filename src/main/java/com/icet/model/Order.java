c17f14f1a3b079c4da9e34923bbd1be1dcaa0931
Sandaruwan Wadiyage <wadiyage567@gmail.com>
2026-01-01 22:52:22 +0530

    Add Order entity and JPA repository (Spring Boot, MySQL

---
+++ src/main/java/com/icet/model/Order.java
@@ -0,0 +1,29 @@
+package com.icet.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "`order`")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id", nullable = false)
+    private Customer customer;
+
+    @Column(name = "order_date", nullable = false)
+    private LocalDateTime orderDate = LocalDateTime.now();
+
+    @Column(name = "total_amount", nullable = false)
+    private Double totalAmount = 0.0;
+
+
+}
