e246ca2c7d2a92c7da1403cccaec4745f46ce6e1
Sandaruwan Wadiyage <wadiyage567@gmail.com>
2026-01-08 17:26:26 +0530

    Feature: Add Swagger/OpenAPI documentation for REST APIs

--- src/main/java/com/icet/ordermanagementsystem/controller/OrderController.java
+++ src/main/java/com/icet/ordermanagementsystem/controller/OrderController.java
@@ -3,6 +3,8 @@ package com.icet.ordermanagementsystem.controller;
 import com.icet.ordermanagementsystem.dto.OrderRequestDTO;
 import com.icet.ordermanagementsystem.dto.OrderResponseDTO;
 import com.icet.ordermanagementsystem.service.OrderService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/orders")
+@Tag(name = "Order API", description = "Operations related to order management")
 public class OrderController {
     private final OrderService orderService;
     public OrderController(OrderService orderService) {
@@ -18,22 +21,26 @@ public class OrderController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Create a new order", description = "Creates a new order and stores in the database")
     public OrderResponseDTO createOrder(@RequestBody OrderRequestDTO dto) {
         return orderService.createOrder(dto);
     }
 
     @GetMapping
+    @Operation(summary = "Get all exiting orders", description = "Get all the exiting orders, those stored in the database")
     public List<OrderResponseDTO> getAllOrders() {
         return orderService.getAllOrders();
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Get any order", description = "Get any order, that stored in the database")
     public OrderResponseDTO getOrderById(@PathVariable Integer id) {
         return orderService.getOrderById(id);
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @Operation(summary = "Delete any exiting order", description = "Delete any exiting order, that stored in the database")
     public void deleteOrder(@PathVariable Integer id) {
         orderService.deleteOrder(id);
     }
--- src/main/java/com/icet/ordermanagementsystem/controller/ProductController.java
+++ src/main/java/com/icet/ordermanagementsystem/controller/ProductController.java
@@ -3,6 +3,8 @@ package com.icet.ordermanagementsystem.controller;
 import com.icet.ordermanagementsystem.dto.ProductRequestDTO;
 import com.icet.ordermanagementsystem.dto.ProductResponseDTO;
 import com.icet.ordermanagementsystem.service.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
+@Tag(name = "Product API", description = "Operations related to product management")
 public class ProductController {
     private final ProductService productService;
 
@@ -19,27 +22,32 @@ public class ProductController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Create a new product", description = "Creates a new product and stores in the database")
     public ProductResponseDTO createProduct(ProductRequestDTO dto) {
         return productService.createProduct(dto);
     }
 
     @GetMapping
+    @Operation(summary = "Get all exiting products", description = "Get all the products, those stored in the database")
     public List<ProductResponseDTO> getAllProducts() {
         return productService.getAllProducts();
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Get existing any product via Product ID", description = "Get existing any product, that stored in the database through its id")
     public ProductResponseDTO getProductId(@PathVariable Integer id) {
         return productService.getProductById(id);
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Update any existing product details via Product ID", description = "Update any exiting product details, that stored in the database through its id")
     public ProductResponseDTO updateProduct(@PathVariable Integer id, @RequestBody ProductRequestDTO dto) {
         return productService.updateProduct(id, dto);
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @Operation(summary = "Delete any exiting product via Product ID", description = "Delete any exiting product, that stored in the database through its id")
     public void deleteProduct(@PathVariable Integer id) {
         productService.deleteProduct(id);
     }
